650083a3137f1cb70b2f327365303a3a30d4715a
qiuChengleiy <qcl9701s666@163.com>
2019-06-12 12:57:16 +0800

    commit

--- core/http_.js
+++ core/http_.js
@@ -47,26 +47,28 @@
     // app.listen(8000)
 }
 
-// client    ---- https 解析
+// client    ---- https | http解析
 const https = require('https')
 const fs = require('fs')
 
 {
-    // https.get('https://www.bilibili.com',(res) => {
-    //     console.log(res.headers)
-    //     let data = ''
-    //     res.on('data', (chunk) => {
-    //         data+=chunk.toString()
-    //     })
+    () => {
+    // http
+    https.get('https://www.bilibili.com',(res) => {
+        console.log(res.headers)
+        let data = ''
+        res.on('data', (chunk) => {
+            data+=chunk.toString()
+        })
 
-    //     res.on('end',() => {
-    //         console.log(data)   //  返回html 字符串
-    //     })
-    // })
+        res.on('end',() => {
+            console.log(data)   //  返回html 字符串
+        })
+    })
 
-    // 测试视频下载
-    const url = 'https://www.bilibili.com/video/av50839659/'
-    const ws = fs.createWriteStream('./test.mp4')
+    // 文件下载
+    const url = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560310882335&di=3c11ef2b81970197eac6c607305ddb79&imgtype=0&src=http%3A%2F%2Fpic9.nipic.com%2F20100923%2F2531170_140325352643_2.jpg'
+    const ws = fs.createWriteStream('./test.jpg')
 
     let receivedBytes = 0;
     let totalBytes = 0;
@@ -74,26 +76,105 @@ const fs = require('fs')
     https.get(url,(res) => {
         res.pipe(ws)
 
-        res.on('response', (data) => {
-            // 更新总文件字节大小
-            totalBytes = parseInt(data.headers['content-length'], 10);
-        });
+        totalBytes = parseInt(res.headers['content-length'], 10);
 
         res.on('data', (chunk) => {
             // 更新下载的文件块字节大小
             receivedBytes += chunk.length;
         })
 
+        const timer = setInterval(() =>  console.log('当前进度---->'+((receivedBytes/totalBytes)*100).toFixed(2) + '%'),0)
+
         ws.on('finish',() => {
             console.log('写入完成')
+            clearInterval(timer)
         })
 
         res.on('end',() => {
-            console.log(receivedBytes)
-            console.log('写入完毕')   //  返回html 字符串
+            console.log('写入结束')   //  返回html 字符串
+            ws.close()
         })
-    })
+    })}
 
+}
+
+
+// 创建https 服务器
+// https模块与http模块极为类似，区别在于https模块需要额外处理SSL证书
+{
+    () => {
+    const options = {
+        key: fs.readFileSync('./ssl/default.key'),
+        cert: fs.readFileSync('./ssl/default.cer')
+    };
+
+    const server = https.createServer(options, function (request, response) {
+            // ...
+    });
+
+    // NodeJS支持SNI技术，可以根据HTTPS客户端请求使用的域名动态使用不同的证书，因此同一个HTTPS服务器可以使用多个域名提供服务。接着上例，可以使用以下方法为HTTPS服务器添加多组证书。
+    server.addContext('foo.com', {
+        key: fs.readFileSync('./ssl/foo.com.key'),
+        cert: fs.readFileSync('./ssl/foo.com.cer'),
+        rejectUnauthorized: false
+        // 如果目标服务器使用的SSL证书是自制的，不是从颁发机构购买的，默认情况下https模块会拒绝连接，提示说有证书安全问题。
+        //在options里加入rejectUnauthorized: false字段可以禁用对证书有效性的检查，从而允许https模块请求开发环境下使用自制证书的HTTPS服务器
+    });
+    
+    server.addContext('bar.com', {
+        key: fs.readFileSync('./ssl/bar.com.key'),
+        cert: fs.readFileSync('./ssl/bar.com.cer')
+    });
+
+
+    // client
+    const options_ = {
+        hostname: 'www.example.com',
+        port: 443,
+        path: '/',
+        method: 'GET'
+    };
+
+    const request = https.request(options_, function (response) {});
+
+    request.end()}
+}
+
+
+// url 解析
+const url = require('url')
+const queryString = require('querystring')
+{
+
+    http.createServer((req,res) => {
+        const reqUrl = req.url
+        const _ = url.parse(reqUrl)  // url解析  --- 有用的字段为   *** query    pathname
+
+        res.writeHead(200,{ 'Content-Type': 'text/plain' })
+        res.end(JSON.stringify(_))
+        //{"protocol":null,
+        //   "slashes":null,
+        //   "auth":null,
+        //   "host":null,
+        //   "port":null,
+        //   "hostname":null,
+        //   "hash":null,
+        //   "search":"?a=1",
+        //   "query":"a=1",
+        //   "pathname":"/test/user",
+        //   "path":"/test/user?a=1",
+        //   "href":"/test/user?a=1"}
+
+        // 转成url
+        const url_ = url.format({protocol: 'http',host: 'www.baidu.com',pathname: '/search',search: 'a=1'})
+        console.log(url_)
+        // http://www.baidu.com/search?a=1
+
+        // query参数解析
+        const query = queryString.parse('a=1&a=100&a=50&b=2')
+        console.log(query) // { a: [ '1', '100', '50' ], b: '2' }
+
+    }).listen(8000)
 }
 
 
